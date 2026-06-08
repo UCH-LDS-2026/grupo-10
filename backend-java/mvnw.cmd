555e97e468191d63b5998453abc898f13288ca30
candeborodijferrari-lang <candeborodijferrari@gmail.com>
2026-06-08 19:56:55 -0300

    Revert "Merge pull request #22 from UCH-LDS-2026/feat/tests-jacoco"
    This reverts commit 4584a352c7b5fe9c2a50c5b77b25ac1e06a14a82, reversing
    changes made to 2ae6e295e1a63c23f3aa72621021e87e13154469.

--- backend-java/mvnw.cmd
+++ backend-java/mvnw.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "MVN_EXE=%~dp0apache-maven-3.9.6\bin\mvn.cmd"
+set "MVN_EXE=C:\Users\santi\.m2\wrapper\dists\apache-maven-3.9.11\03d7e36a140982eea48e22c1dcac01d8862b2550b2939e09a0809bbc5182a5bc\bin\mvn.cmd"
 
 if not exist "%MVN_EXE%" (
     echo ERROR - No se encontro Maven en la ruta esperada.
--- backend-java/src/main/resources/db/migration/V2__increase_atraccion_id_length.sql
+++ backend-java/src/main/resources/db/migration/V2__increase_atraccion_id_length.sql
@@ -1,4 +1,1 @@
-ALTER TABLE itinerario_items DROP FOREIGN KEY fk_atraccion;
-ALTER TABLE atracciones MODIFY id VARCHAR(100);
-ALTER TABLE itinerario_items MODIFY atraccion_id VARCHAR(100);
-ALTER TABLE itinerario_items ADD CONSTRAINT fk_atraccion FOREIGN KEY (atraccion_id) REFERENCES atracciones(id) ON DELETE RESTRICT;
+ALTER TABLE atracciones MODIFY id VARCHAR(100); ALTER TABLE itinerario_items MODIFY atraccion_id VARCHAR(100);
